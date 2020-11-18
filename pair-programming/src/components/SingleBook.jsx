b8e03fa556cb3a87f482bb2efecebbed5f627815
elinoza <hillcakmak@gmail.com>
2020-11-18 17:51:48 +0300

    Update SingleBook.jsx

--- pair-programming/src/components/SingleBook.jsx
+++ pair-programming/src/components/SingleBook.jsx
@@ -1,16 +1,25 @@
 import React from "react";
 import { Card, Button } from "react-bootstrap";
 
-const SingleBook = (props) => {
-  return (
-    <Card style={{ width: "18rem" }} className="mx-auto">
-      <Card.Img variant="top" src={props.book.img} />
+
+
+class SingleBook extends React.Component {
+  state={
+    selected:false
+  }
+
+  render(){
+    return(
+      <Card style={{ width: "18rem" }} className="mx-auto">
+      <Card.Img variant="top" src={this.props.book.img} />
       <Card.Body>
-        <Card.Title>{props.book.title}</Card.Title>
-        <Card.Text>${props.book.price}</Card.Text>
+        <Card.Title>{this.props.book.title}</Card.Title>
+        <Card.Text>${this.props.book.price}</Card.Text>
       </Card.Body>
     </Card>
-  );
-};
+
+    )
+  }
+}
 
 export default SingleBook;
