1a3714aebbf38c15715fd5817356e17f716ed846
Abdul Samad Mahmood <72197815+ASMahmood@users.noreply.github.com>
2020-11-18 15:09:41 +0000

    ex7 done

--- pair-programming/src/components/SingleBook.jsx
+++ pair-programming/src/components/SingleBook.jsx
@@ -1,24 +1,35 @@
 import React from "react";
 import { Card, Button } from "react-bootstrap";
 
-
-
 class SingleBook extends React.Component {
-  state={
-    selected:false
-  }
+  state = {
+    selected: false,
+  };
 
-  render(){
-    return(
-      <Card style={{ width: "18rem" }} className="mx-auto">
-      <Card.Img variant="top" src={this.props.book.img} />
-      <Card.Body>
-        <Card.Title>{this.props.book.title}</Card.Title>
-        <Card.Text>${this.props.book.price}</Card.Text>
-      </Card.Body>
-    </Card>
+  toggleSelected = () => {
+    if (this.state.selected === false) {
+      this.setState({ selected: true });
+    } else {
+      this.setState({ selected: false });
+    }
+  };
 
-    )
+  render() {
+    return (
+      <Card
+        onClick={() => this.toggleSelected()}
+        style={{ width: "18rem" }}
+        className={
+          this.state.selected === true ? "bg-primary mx-auto" : "mx-auto"
+        }
+      >
+        <Card.Img variant="top" src={this.props.book.img} />
+        <Card.Body>
+          <Card.Title>{this.props.book.title}</Card.Title>
+          <Card.Text>${this.props.book.price}</Card.Text>
+        </Card.Body>
+      </Card>
+    );
   }
 }
 
