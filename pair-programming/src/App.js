19bdc4c952c65457d3df8d1733fde706ba0fea33
elinoza <hillcakmak@gmail.com>
2020-11-18 16:52:57 +0300

    ex2

--- pair-programming/src/App.js
+++ pair-programming/src/App.js
@@ -1,11 +1,14 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import WarningSign from "./components/WarningSign";
+import MyBadge from  "./components/MyBadge";
+
 
 function App() {
   return (
     <div className="App">
       <WarningSign warningText="uh oh stinky" />
+      <MyBadge  color="success" text="wow!"/>
     </div>
   );
 }
